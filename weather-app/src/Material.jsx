7dd106cc9d43a82b97ad397de7f5b9a9dd2dde33
DarshanGTHB <darshanvekaiya25102004@gmail.com>
2025-05-23 13:13:24 +0530

    recovered Material.jsx

---
+++ weather-app/src/Material.jsx
@@ -0,0 +1,40 @@
+// import ButtonGroup from "@mui/material/ButtonGroup";
+// import Button from "@mui/material/Button";
+// import SaveIcon from "@mui/icons-material/Save";
+// import Badge from '@mui/material/Badge';
+// import { styled } from '@mui/material/styles';
+// import IconButton from '@mui/material/IconButton';
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// import { useState } from "react";
+
+
+// const StyledBadge = styled(Badge)(({ theme }) => ({
+//   '& .MuiBadge-badge': {
+//     right: -3,
+//     top: 0,
+//     border: `2px solid ${(theme.vars ?? theme).palette.background.paper}`,
+//     padding: '0 4px',
+//   },
+// }));
+
+  // let [cnt, setCnt] = useState(1);
+  // let handleClick = () => {
+  //   setCnt( cnt => cnt+1 );
+  // }
+
+
+
+
+{/* <Button variant="contained">Hello world</Button>
+      <ButtonGroup variant="outlined" aria-label="Loading button group">
+        <Button>Submit</Button>
+        <Button>Fetch data</Button>
+        <Button loading loadingPosition="start" startIcon={<SaveIcon />}>
+          Save
+        </Button>
+      </ButtonGroup>
+      <IconButton aria-label="cart" onClick={handleClick}>
+        <StyledBadge badgeContent={cnt} color="secondary">
+          <ShoppingCartIcon />
+        </StyledBadge>
+      </IconButton> */}
